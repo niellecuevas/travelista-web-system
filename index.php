0b9b0b3549fe5e87a1c34a63a3ea792cbe77f1e6
SofiaMaeP <supiimeylanto@gmail.com>
2023-11-28 16:15:17 +0800

    added topbar and logo

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
-<?php include '../websystem/components/header.php';?>
-
 <!DOCTYPE html>
 <html>
 <head>
     <title>User Page</title>
     <link rel="stylesheet" href="css/index.css">
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto+Condensed&display=swap" rel="stylesheet">
 </head>
 <body>
 <!-- Top Bar -->
+    <section id="top-bar">   
+        <div id="logo">
+            <img src="images/logoround.png">
+        </div>
+        <div id="title">
+            <h1>Travelista</h1>
+        </div>
+    </section>
 </body>
 </html>
