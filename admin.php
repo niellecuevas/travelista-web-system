4d32ae76c06b27126c02f366bef169aef7f51c25
niellecuevas <raniella810@gmail.com>
2023-11-28 16:17:40 +0800

    added booking table in admin

--- admin.php
+++ admin.php
@@ -1,5 +1,3 @@
-<?php include '../websystem/components/header.php';?>
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +5,16 @@
     <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
-<!-- Top Bar -->
+<?php include '../websystem/components/header.php';?>
+<div class="bookingList-container">
+    <h1>Bookings</h1>
+        <table>
+            <tr class="tbl_bookingList">
+                <th>Name</th>
+                <th>Date</th>
+                <th>Payment Status</th>
+            </tr>
+    </table>
+</div>
 </body>
 </html>
