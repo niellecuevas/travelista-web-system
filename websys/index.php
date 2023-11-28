e27fcbc4e2ba9468d288c149904976b8f6de4685
Sofia Mae Pepito <144592381+SofiaMaeP@users.noreply.github.com>
2023-11-28 15:05:13 +0800

    Add files via upload

---
+++ websys/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Page</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+<!-- Top Bar -->
+    <section id="top-bar">   
+        <div class="logo">
+            <img src="images/logoupdated.png">
+            <h1>Travelista</h1>
+        </div>
+    </section>
+</body>
+</html>
