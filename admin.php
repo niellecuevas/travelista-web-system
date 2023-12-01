8750ce5ada14032749b2915db98b935cff03d244
Sofia Mae Pepito <144592381+SofiaMaeP@users.noreply.github.com>
2023-12-01 14:32:24 +0800

    sample

--- admin.php
+++ admin.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin</title>
-    <link rel="icon" href="images/logoupdated.png" sizes="32x32" type="image/png">
     <link rel="stylesheet" href="css/admin.css">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Roboto+Condensed&display=swap" rel="stylesheet">
 </head>
 <body>
-
 <?php include '../websystem/components/header.php';?>
-
 <div class="bookingList-container">
     <h1 class="booking-title">BOOKINGS</h1>
     <!-- Sorting Dropbox -->
--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Travelista</title>
-    <link rel="icon" href="images/logoupdated.png" sizes="32x32" type="image/png">
+    <title>User Page</title>
     <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/userform.css">
     <!-- fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,15 +23,9 @@
 
         <button class="btncss">Book Now!</button>
 
+        <?php include '../websystem/components/userform.php';?>
+
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam dapibus neque dui. In vulputate a diam eget accumsan. Aliquam nec nibh enim. Curabitur eget eros faucibus, pretium libero ac, ornare arcu. Vestibulum tristique felis eu auctor rutrum. Quisque dictum eros convallis odio ultrices pharetra. Duis metus dolor, scelerisque in felis eu, vestibulum tincidunt orci. Praesent non nisi nec est sollicitudin condimentum. Phasellus condimentum lacus eu pharetra vestibulum. Fusce mollis lacinia nisl aliquam faucibus. Maecenas nulla augue, fermentum eget turpis id, commodo tincidunt arcu. In ultrices ut sapien id malesuada. Maecenas tempus odio ac enim rhoncus feugiat.
-
-        Maecenas a condimentum metus, ac dignissim magna. Donec nec nisi non quam commodo mattis eu a justo. Etiam ultrices lorem vel mi varius, nec pellentesque nisi aliquet. Vestibulum malesuada turpis ut turpis laoreet interdum. Suspendisse sit amet bibendum nibh. Nam eu lorem sagittis, aliquam est eu, malesuada metus. Pellentesque pulvinar at lacus quis consequat. Vestibulum viverra lacus sed purus mollis, interdum consectetur quam porttitor. Integer sodales venenatis metus, efficitur sodales orci blandit a. Maecenas in felis felis. Vestibulum ut rhoncus tellus.
-
-        Curabitur accumsan iaculis justo vel dictum. Maecenas rutrum diam risus, at congue erat pretium in. Proin viverra nec quam sed placerat. Praesent sollicitudin rhoncus interdum. Praesent hendrerit pellentesque maximus. Maecenas porta at erat a interdum. Quisque faucibus suscipit pharetra. Donec vel nibh a metus pharetra feugiat. Sed finibus blandit pulvinar.
-
-        Nunc quis sem a libero dictum sollicitudin id at nunc. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean rhoncus ullamcorper pellentesque. Sed eu urna a lorem pretium porta eget eget massa. Phasellus egestas diam mi, id cursus ligula iaculis eu. Aenean eget venenatis ipsum. Quisque a elit sagittis, maximus sapien ut, auctor est. Morbi pharetra volutpat tincidunt. Sed cursus ex nisl, tincidunt porta diam commodo id. Vivamus tempus eros eu mauris tincidunt, vel lacinia orci posuere. Donec consectetur ornare odio, eu hendrerit lorem interdum sed. Morbi sagittis feugiat aliquam. Etiam vel gravida justo. Suspendisse iaculis augue odio, quis egestas leo scelerisque a. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-
         Etiam nibh quam, tincidunt id velit vitae, vehicula volutpat tellus. Sed vel orci eu ante luctus euismod faucibus ac erat. Aenean eget eleifend ipsum, eu rhoncus sapien. Aliquam congue finibus faucibus. Donec erat purus, bibendum eu felis id, dictum convallis tellus. Duis at quam quam. In condimentum elementum enim. Nulla in metus commodo enim euismod posuere quis vitae sem. Nam sed pellentesque lectus, id auctor orci. Praesent condimentum lectus feugiat orci vulputate, nec condimentum arcu eleifend. Aenean mollis convallis ex, quis elementum lectus egestas ac. Praesent viverra nisi diam, vitae pretium arcu posuere non. Mauris ornare urna elit, nec lacinia dui mollis sed. Cras consectetur, orci eget posuere cursus, diam ligula lobortis lectus, sit amet dictum mi risus sit amet velit. Donec imperdiet feugiat tortor sit amet imperdiet.
         </p>
     </div>
